198bc644756ed63329a014cf9bc02ee6e1e42f11
Max Sánchez <edmax86@hotmail.com>
2021-10-20 07:40:52 -0500

    Hola Tu. Agregado a views

--- django/hola/principal/urls.py
+++ django/hola/principal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.holaDjango, name="holaDjango"),
-    path('pepe', views.pepe, name='holaPepe')
+    path('pepe', views.pepe, name='holaPepe'),
+    path('<str:nombre>', views.holaTu, name='holaTu')
     ]
