78023e212265dc07289a083f9b64eb0b074b034f
Max Sánchez <edmax86@hotmail.com>
2021-10-26 08:11:27 -0500

    Es viernes v2.1

--- django/hola/dia/views.py
+++ django/hola/dia/views.py
@@ -5,4 +5,5 @@ from datetime import datetime
 def index(request):
     return render(request, 'dia/index.html', {
         'esViernes': datetime.today().isoweekday() == 5
+        #'esViernes': 'Ya es viernes' if datetime.today().isoweekday() == 2 else 'No es viernes'
     })
