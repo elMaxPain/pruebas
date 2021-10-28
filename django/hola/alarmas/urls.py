b9634403d0da669aead24dea68643fb59c31fb23
Max Sánchez <edmax86@hotmail.com>
2021-10-28 08:00:12 -0500

    Alarmas v2 y v2.1

--- django/hola/alarmas/urls.py
+++ django/hola/alarmas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'alarmas'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("v2", views.v2, name="v2"),
--- django/hola/principal/urls.py
+++ django/hola/principal/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'principal'
+
 urlpatterns = [
     path("", views.holaDjango, name="holaDjango"),
     path('pepe', views.pepe, name='holaPepe'),
     path('indice', views.indice, name='indice'),
     path('indice/<str:nombre>', views.indiceParam, name='indiceParam'),
     path('<str:nombre>', views.holaTu, name='holaTu')
-    ]
+]
