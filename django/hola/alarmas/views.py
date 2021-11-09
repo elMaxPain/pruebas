5d74f8ac8f12cfaa1c6f821610dac5c37b60133c
Max Sánchez <edmax86@hotmail.com>
2021-11-09 08:07:05 -0600

    Alarmas v3.1

--- django/hola/alarmas/views.py
+++ django/hola/alarmas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 
 alarmas = ['5:30', '5:35', '5:40', '5:50']
 
@@ -8,4 +9,10 @@ def index(request):
     return render(request, 'alarmas/index.html', {'alarmas':alarmas})
 
 def v2(request):
-    return render(request, 'alarmas/v2.html')
+    return render(request, 'alarmas/v2.html', {'cont_form':FNuevaAlarma()})
+
+class FNuevaAlarma(forms.Form): # hereda
+    alarma = forms.CharField(label='Nueva alarma')  # Campo de caracteres
+                                                    # con etiqueta
+                                                    # <input type='text' ...>
+    #snooze = forms.IntegerField(label='Repetir', min_value=0, max_value=10)
